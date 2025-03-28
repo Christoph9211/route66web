b3620e30bf82cbeeef245bd996190532d8b44394
Christopher Gibbons <chrisgibbons9211@gmail.com>
2025-03-28 01:00:42 -0500

    Update product availability message and correct item name for consistency

--- app.js
+++ app.js
@@ -284,7 +284,7 @@ function App() {
               </div>
             ) : (
               <div className="col-span-full text-center py-12">
-                <p className="text-gray-700 dark:text-gray-300">No products found in this category.</p>
+                <p className="text-gray-700 dark:text-gray-300">Products Coming Soon</p>
               </div>
             )}
           </div>
@@ -516,7 +516,7 @@ function App() {
                     </li>
                     <li>
                       <a href="#" className="text-base text-gray-400 hover:text-white">
-                        Hemp Flowers
+                        Hemp Flower
                       </a>
                     </li>
                     <li>
