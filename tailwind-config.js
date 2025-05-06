86db151b64fc146719d20125bb472779cdea159c
Christoph9211 <chrisgibbons9211@gmail.com>
2025-05-06 01:39:03 -0500

    format documents with prettier

--- tailwind-config.js
+++ tailwind-config.js
@@ -1,15 +1,15 @@
 tailwind.config = {
-  darkMode: 'media',
+  darkMode: "media",
   theme: {
     extend: {
       colors: {
-        primary: '#0F0F9A', // Darkened for better contrast
-        secondary: '#45b34b', // Darkened for better contrast
-        accent: '#3A9A2A' // Updated accent color for better contrast
+        primary: "#0F0F9A", // Darkened for better contrast
+        secondary: "#45b34b", // Darkened for better contrast
+        accent: "#3A9A2A", // Updated accent color for better contrast
       },
       fontFamily: {
-        sans: ['Montserrat', 'sans-serif']
-      }
-    }
-  }
-}
+        sans: ["Montserrat", "sans-serif"],
+      },
+    },
+  },
+};
