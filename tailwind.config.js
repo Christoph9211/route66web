608f2ae21a0960f0a4e0831ad68db7859a59544c
Christoph9211 <chrisgibbons9211@gmail.com>
2025-07-15 16:35:28 -0500

    allowing tailwind cdn

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// {import('tailwindcss').Config} 
+tailwind.config = {
+  darkMode: "media",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#1D4ED8", // Modern blue primary color
+        secondary: "#10B981", // Modern green secondary color
+        accent: "#FBBF24", // Warm accent for highlights
+      },
+      fontFamily: {
+        sans: ["Inter", "Montserrat", "sans-serif"],
+      },
+    },
+  },
+};
