3f82a07f54ca02acc58438d026cb0398cfcadf74
Christoph9211 <chrisgibbons9211@gmail.com>
2025-07-31 02:23:27 -0500

    removed terser

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,13 +4,5 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [
     react(),
-  ],
-  build: {
-    minify: 'terser',
-    terserOptions: {
-      compress: {
-        drop_console: true,
-      },
-    },
-  },
+  ]
 });
