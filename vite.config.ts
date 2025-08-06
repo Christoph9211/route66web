069076ff48a7e405c70cf6ed93375ec5efbe0559
Christoph9211 <chrisgibbons9211@gmail.com>
2025-08-05 21:46:05 -0500

    got rid of unused dependencies and completly redid the vite and typescript config files

--- vite.config.ts
+++ vite.config.ts
@@ -1,26 +1,8 @@
-// vite.config.ts
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// import { resolve } from 'node:path';
-import { fileURLToPath } from 'url';
-import { dirname } from 'path';
 
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
-    build: {
-        // outDir defaults to "dist".  Keep or change as you like.
-        rollupOptions: {
-          input: {
-            
-            main:  __dirname + '/index.html',         
-            terms: __dirname + '/terms-of-service.html', 
-            privacy: __dirname + '/privacy-policy.html'
-            
-          }
-        }
-      }
-});
+  plugins: [react(), tailwindcss()],
+})
