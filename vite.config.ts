b78b049d885174bee52e0d100cc319c1d34838d2
Christoph9211 <chrisgibbons9211@gmail.com>
2025-08-05 22:24:42 -0500

    fixed links finally

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,14 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-})
+  build: {
+    // outDir defaults to "dist".  Keep or change as you like.
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        terms: 'src/terms-of-service.html',
+        privacy: 'src/privacy-policy.html'
+      }
+    }
+  }
+});
